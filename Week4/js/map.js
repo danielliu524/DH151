34fc227b62cd7465ccf1e0ef30de2d58c3b31c0c
danielliu524 <47194494+danielliu524@users.noreply.github.com>
2021-04-26 01:54:55 +0800

    Week 4

---
+++ Week4/js/map.js
@@ -0,0 +1,85 @@
+// Global variables
+let map;
+let lat = 0;
+let lon = 0;
+let zl = 3;
+let hidden = false;
+// path to csv data
+let path = "data/silicon-valley-companies.csv";
+let markers = L.featureGroup();
+
+// initialize
+$( document ).ready(function() {
+	createMap(lat,lon,zl);
+	readCSV(path);
+});
+
+// create the map
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+// function to read csv data
+function readCSV(path){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			// map the data
+			mapCSV(data);
+		}
+	});
+}
+
+function mapCSV(data){
+
+    // circle options
+    let circleOptions = {
+        radius: 5,
+        weight: 1,
+        color: 'white',
+        fillColor: 'dodgerblue',
+        fillOpacity: 1
+    }
+
+    // loop through each entry
+    data.data.forEach(function(item,index){
+        // create a marker
+        let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+        .on('mouseover',function(){
+            this.bindPopup(`<h2>${item.name}</h2><p>${item.description}</p>`).openPopup()
+        })
+        // add marker to featuregroup
+        markers.addLayer(marker)
+
+        // add entry to sidebar
+        $('.sidebar').append(`<div class="sidebar-item" onmouseover="panToImage(${index})" onclick="window.open('http://${item.website}')">${item.name}</div>`)
+    })
+
+    // add featuregroup to map
+    markers.addTo(map)
+
+    // fit map to markers
+    map.fitBounds(markers.getBounds())
+}
+
+function panToImage(index){
+	// zoom to level 17 first
+	map.setZoom(17);
+	// pan to the marker
+	map.panTo(markers.getLayers()[index]._latlng);
+}
+
+function showHideSidebar() {
+    if(hidden) {
+        document.body.style.gridTemplateColumns = "250px 1fr";
+    }
+    else {
+        document.body.style.gridTemplateColumns = "0px 1fr";
+    }
+    hidden = !hidden
+}
